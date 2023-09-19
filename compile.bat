c5406d159a1632bfe9938bb52ac765a2582ba171
Imiloin <finvoler6@gmail.com>
2023-09-19 20:34:52 +0800

    first commit

---
+++ compile.bat
@@ -0,0 +1,6 @@
+@echo off
+del *.aux *.out *.xml *.thm *.toc *.lof *.lot *.bbl *.bcf *.blg *.idx *.ind *.log *.gz
+xelatex -shell-escape main.tex
+biber main.bcf
+xelatex -shell-escape main.tex
+xelatex -shell-escape main.tex
