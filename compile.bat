ec9e57ac49eef3b8a60e120167fe52d0840751a6
Imiloin <finvoler6@gmail.com>
2023-10-07 21:59:56 +0800

    start Chap 3

--- compile.bat
+++ compile.bat
@@ -1,5 +1,5 @@
 @echo off
-del *.aux *.out *.xml *.thm *.toc *.lof *.lot *.bbl *.bcf *.blg *.idx *.ind *.log *.gz
+del *.aux *.out *.xml *.thm *.toc *.lof *.lot *.bbl *.bcf *.blg *.idx *.ind *.log *.gz *.gnuplot
 xelatex -shell-escape main.tex
 biber main.bcf
 xelatex -shell-escape main.tex
