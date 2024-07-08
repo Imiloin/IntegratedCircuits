96088fcc69d0431f09ba533462dd7943149be518
Imiloin <finvoler6@gmail.com>
2024-07-08 11:45:04 +0800

    update .gitignore and add .gitattributes

--- compile.bat
+++ compile.bat
@@ -1,5 +1,5 @@
 @echo off
-del *.acn *.acr *.alg *.aux *.bbl *.bcf *.blg *.fdb_latexmk *.fls *.glg *.glo *.gls *.gnuplot *.idx *.ind *.ist *.lof *.log *.lot *.out *.thm *.toc *.run.xml *.synctex.gz
+del *.acn *.acr *.alg *.aux *.bbl *.bcf *.blg *.fdb_latexmk *.fls *.glg *.glo *.gls *.gnuplot *.idx *.ind *.ist *.lof *.log *.lot *.out *.thm *.toc *.upa *.upb *.run.xml *.synctex.gz
 xelatex -shell-escape main.tex
 biber main.bcf
 xelatex -shell-escape main.tex
